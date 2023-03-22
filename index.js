f6f5e7c66dbda861be1e5e7ef03c35cf90466463
Ebadzmn <ebadzmn1@gmail.com>
2023-03-22 15:50:03 +0600

    change

---
+++ index.js
@@ -0,0 +1,12 @@
+function multiplicationTable(num) {
+
+    for (var i = 1; i <= 10; i++) {
+
+      var product = num * i;
+
+      console.log(num + " x " + i + " = " + product);
+    }
+
+  }
+  multiplicationTable(2);
+  multiplicationTable(8);
